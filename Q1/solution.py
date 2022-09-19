2606da9536436cd9239351f49b864e317a1ce427
AbubakarSiddiqueQC <abubakarsiddique.qc@gmail.com>
2022-09-19 22:10:21 +0500

    Changed the signatures of the function.

--- Q1/solution.py
+++ Q1/solution.py
@@ -1,5 +1,5 @@
 ## Add code below with answer clearly stated
-def sum_of_factorial_digit(n)->int:
+def sum_of_factorial_digit(n: int)->int:
   """ This function will take an argument that is number it will find its factorial and treat that as string to find the sum of the digits in the factorial """
   fact = 1
   sum = 0
